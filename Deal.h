751cb1850b2f1f1c0575cb686a51eece2392acc6
rafadona <hadjadjrafik@gmail.com>
2023-06-25 16:52:54 +0200

    Add Class Deal

---
+++ Deal.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef Deal_h
+#define Deal_h
+
+class Deal {
+private:
+
+
+public:
+  Deal( ){
+    
+  }
+
+
+
+};
+
+
+#endif /* Deal_h */
