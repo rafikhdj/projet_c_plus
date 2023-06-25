b8e84efa88ff6bf6b430478ef718b56a619ad360
rafadona <hadjadjrafik@gmail.com>
2023-06-25 16:57:26 +0200

    Add class Lenders.h

---
+++ Lenders.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef Lenders_h
+#define Lenders_h
+
+class Lenders {
+private:
+
+
+public:
+  Lenders( ){
+    
+  }
+
+
+
+};
+
+
+#endif /* Lenders_h */
